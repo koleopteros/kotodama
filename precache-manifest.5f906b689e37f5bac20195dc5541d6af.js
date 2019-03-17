674490bf559e0283601368ab97430bfc14301131
JC <32308641+Tearamissyoucake@users.noreply.github.com>
2019-03-17 12:59:27 -0400

    Updates

---
+++ precache-manifest.5f906b689e37f5bac20195dc5541d6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kotodama/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5a602bff0c6a19fd64ff",
+    "url": "/kotodama/static/js/runtime~main.571fa706.js"
+  },
+  {
+    "revision": "77e3f7b94915fe2be6d5",
+    "url": "/kotodama/static/js/main.41310a2b.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/kotodama/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "77e3f7b94915fe2be6d5",
+    "url": "/kotodama/static/css/main.0edbf656.chunk.css"
+  },
+  {
+    "revision": "8a9e20a874602cb0a190758cb58e9ba5",
+    "url": "/kotodama/index.html"
+  }
+];
